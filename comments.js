f844d346c76163a12c8c1ec5b5a96f84c469cae4
Mert Arısoy <mertarisoy@users.noreply.github.com>
2024-05-06 21:51:58 +0000

    Copilot third commit

---
+++ comments.js
@@ -0,0 +1,9 @@
+
+// Create web server for comment
+//========================================
+
+const express = require('express');
+const router = express.Router();
+const commentController = require()
+
+router.get('/', commentController.comment_list);
